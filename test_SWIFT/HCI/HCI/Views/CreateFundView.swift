6884288adeb87aa6fb34dd5a675233a614e3b56d
Leonardo Scappatura <scappatura.1984992@studenti.uniroma1.it>
2024-06-05 15:12:43 +0200

    Update CreateFundView

--- test_SWIFT/HCI/HCI/Views/CreateFundView.swift
+++ test_SWIFT/HCI/HCI/Views/CreateFundView.swift
@@ -9,24 +9,107 @@ struct CreateFundView: View {
     @State private var friends: [String] = []
     @State private var newFriend: String = ""
     
+    @State var screenWidth: CGFloat = UIScreen.main.bounds.width
+    @State var screenHeight: CGFloat = UIScreen.main.bounds.height
+    
     var body: some View {
         VStack {
-            Text("Create New Fund")
-                .font(.largeTitle)
-                .padding()
+            ZStack{
+                RoundedRectangle(cornerRadius: 14)
+                .foregroundColor(Color.blue)
+                Text("Create New Fund")
+                    .font(.largeTitle)
+                    .padding()
+                    .foregroundColor(Color.white)
+                    .fontWeight(.semibold)
+            }
+            .frame(height:200)
+            .padding(.top, -50)
+            ZStack {
+                RoundedRectangle(cornerRadius: 14)
+                    .foregroundColor(Color.white)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 14)
+                            .stroke(Color.white, lineWidth: 2)
+                    )
+                    .shadow(color: Color.black.opacity(0.2), radius: 10, x: 0, y: 3)
+                VStack(alignment: .leading, spacing: 5) {
+                    Text("Fund Name")
+                        .font(.title3)
+                        .fontWeight(.semibold)
+                        .padding(.leading)
+                        .foregroundColor(Color.black)
+                    
+                    ZStack(alignment: .trailing) {
+                        TextField("Fuerteventura's Trip", text: $fundName)
+                            .padding()
+                            .background(Color(.systemGray6))
+                            .cornerRadius(10)
+                            .padding(.horizontal)
+                        
+                        Button(action: {
+                            fundName = ""
+                        }) {
+                            Image(systemName: "xmark.circle.fill")
+                                .foregroundColor(.gray)
+                                .padding(.trailing, 10)
+                        }
+                        .padding(.horizontal)
+                    }
+                }
+                .padding(.horizontal, 5)
+                .padding(.vertical, 10)
+            }
+            .frame(width: screenWidth - 28, height: 86)
+            .padding(.horizontal, 14)
+            .padding(.vertical, 10)
             
-            TextField("Fund Name", text: $fundName)
-                .padding()
-                .background(Color(.systemGray6))
-                .cornerRadius(10)
-                .padding(.horizontal)
-            
-            TextField("Total amount to save", text: $totalAmount)
-                .padding()
-                .background(Color(.systemGray6))
-                .cornerRadius(10)
-                .padding(.horizontal)
-                .keyboardType(.numberPad)
+            ZStack {
+                RoundedRectangle(cornerRadius: 14)
+                    .foregroundColor(Color.white)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 14)
+                            .stroke(Color.white, lineWidth: 2)
+                    )
+                    .shadow(color: Color.black.opacity(0.2), radius: 10, x: 0, y: 3)
+                VStack(alignment: .leading, spacing: 5) {
+                    Text("Total amount to save")
+                        .font(.title3)
+                        .fontWeight(.semibold)
+                        .padding(.leading)
+                        .foregroundColor(Color.black)
+                    
+                    ZStack(alignment: .trailing) {
+                        HStack(spacing: 1) {
+                            Text("€")
+                                .fontWeight(.bold)
+                                .padding(.leading, 1.0)
+                            
+                            TextField("6000", text: $totalAmount)
+                                .padding()
+                                .background(Color(.systemGray6))
+                                .cornerRadius(10)
+                                .padding(.horizontal, 5)
+                        }
+                        .padding(.leading)
+                    
+                        Button(action: {
+                            totalAmount = ""
+                        }) {
+                            Image(systemName: "xmark.circle.fill")
+                                .foregroundColor(.gray)
+                                .padding(.trailing, 10)
+                        }
+                        .padding(.horizontal)
+                    }
+                    .padding(.trailing, 14)
+                }
+                .padding(.horizontal, 5)
+                .padding(.vertical, 10)
+            }
+            .frame(width: screenWidth - 28, height: 86)
+            .padding(.horizontal, 14)
+            .padding(.vertical, 10)
             
             DatePicker("Start Date", selection: $startDate, displayedComponents: .date)
                 .padding(.horizontal)
