fd139b35fa1322701903fbf6d4c06179fea86869
Leonardo Scappatura <95079472+Leonard013@users.noreply.github.com>
2024-06-07 10:18:51 +0200

    Button fixed

--- test_SWIFT/HCI/HCI/Views/GroupsView.swift
+++ test_SWIFT/HCI/HCI/Views/GroupsView.swift
@@ -7,73 +7,71 @@ struct GroupsView: View {
     
     var body: some View {
         NavigationView {
-            VStack {
-                ZStack{
-                    RoundedRectangle(cornerRadius: 14)
-                    .foregroundColor(Color.blue)
-                    Text("Groups")
-                        .font(.largeTitle)
-                        .padding()
-                        .foregroundColor(Color.white)
-                        .fontWeight(.semibold)
-                }
-                .frame(height:200)
-                .padding(.top, -50)
-                
-                
-                List(viewModel.groups) { group in
-                    NavigationLink(destination: FundDetailsView(group: group)) {
-                        HStack {
-                        VStack(alignment: .leading) {
-                            Text(group.name)
-                                .font(.system(size: 25))
-                                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-                            HStack{
-                                Text("\(group.startDate) --> \(group.endDate)")
-                                    .font(.subheadline)
-                                    .foregroundColor(.gray)
-                                Spacer()
-                                Text("\(group.currentAmount, specifier: "%.0f")/\(group.totalAmount, specifier: "%.0f")€")
-                                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-                                    .font(.system(size: 17))
-                            }
-                            ProgressBar(value: group.currentAmount / group.totalAmount)
-                                .scaleEffect(CGSize(width: 1.0, height: 0.8))
-                                .cornerRadius(14)
-                            }
-
-                            
-                            
-                        
-                    }
-                    }
-                }
-                .listStyle(DefaultListStyle())
-                
-                Spacer()
-                
-                // Add a floating button at the bottom right
-                HStack {
-                    Spacer()
-                    Button(action: {
-                        showingCreateFundView = true
-                    }) {
-                        Image(systemName: "plus")
+            ZStack {
+                VStack {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 14)
+                            .foregroundColor(Color.blue)
+                        Text("Groups")
+                            .font(.largeTitle)
                             .padding()
-                            .background(Color.blue)
-                            .foregroundColor(.white)
-                            .cornerRadius(10)
-                            .shadow(radius: 5)
+                            .foregroundColor(Color.white)
+                            .fontWeight(.semibold)
                     }
-                    .padding()
-                    .sheet(isPresented: $showingCreateFundView) {
-                        CreateFundView()
+                    .frame(height: 200)
+                    .padding(.top, -50)
+
+                    List(viewModel.groups) { group in
+                        NavigationLink(destination: FundDetailsView(group: group)) {
+                            HStack {
+                                VStack(alignment: .leading) {
+                                    Text(group.name)
+                                        .font(.system(size: 25))
+                                        .fontWeight(.bold)
+                                    HStack {
+                                        Text("\(group.startDate) --> \(group.endDate)")
+                                            .font(.subheadline)
+                                            .foregroundColor(.gray)
+                                        Spacer()
+                                        Text("\(group.currentAmount, specifier: "%.0f")/\(group.totalAmount, specifier: "%.0f")€")
+                                            .fontWeight(.bold)
+                                            .font(.system(size: 17))
+                                    }
+                                    ProgressBar(value: group.currentAmount / group.totalAmount)
+                                        .scaleEffect(CGSize(width: 1.0, height: 0.8))
+                                        .cornerRadius(14)
+                                }
+                            }
+                        }
+                    }
+                    .listStyle(DefaultListStyle())
+                }
+                .background(Color(hex: "ECECEE"))
+                .navigationTitle("Groups")
+                .navigationBarHidden(true) // Hide the back button
+
+                VStack {
+                    Spacer()
+                    HStack {
+                        Spacer()
+                        Button(action: {
+                            showingCreateFundView = true
+                        }) {
+                            Image(systemName: "plus")
+                                .padding()
+                                .background(Color.blue)
+                                .foregroundColor(.white)
+                                .cornerRadius(10)
+                                .shadow(radius: 5)
+                        }
+                        .padding()
+                        .sheet(isPresented: $showingCreateFundView) {
+                            CreateFundView()
+                        }
                     }
                 }
+                .padding()
             }
-            .background(Color(hex: "ECECEE"))
-            .navigationTitle("Groups")
-            .navigationBarHidden(true) // Hide the back button
         }
     }
 }
