381a91f580f0c175d9313a9255e11d064958bdb1
Andrea <andrea.salinetti1@icloud.com>
2024-06-09 17:56:55 +0200

    Modified HomePage

--- test_SWIFT/HCI/HCI/Views/HomePage.swift
+++ test_SWIFT/HCI/HCI/Views/HomePage.swift
@@ -6,7 +6,7 @@ struct HomePage: View {
     var body: some View {
         NavigationView {
             VStack {
-                VStack {
+                VStack(spacing:0) {
                     ZStack {
                         RoundedRectangle(cornerRadius: 14)
                             .foregroundColor(Color.blue)
@@ -14,43 +14,62 @@ struct HomePage: View {
                         VStack {
                             Spacer() // Pushes the content to the bottom
                             VStack(alignment: .leading) {
-                                Text("Bentornato Silvio")
-                                    .font(.largeTitle)
-                                    .foregroundColor(.white)
-                                    .bold()
                                 
-                                Spacer()
+                                HStack{
+                                    Spacer()
+                                    VStack(spacing: 0){
+                                        Image(systemName: "creditcard.circle")
+                                            .resizable()
+                                            .foregroundColor(.white)
+                                            .frame(width: 50, height: 50)
+                                        Text("Card")
+                                            .foregroundColor(.white)
+                                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                                            .padding(0)
+                                    }.padding(.top, -30)
+
+                                }
                                     .frame(height: 15.0)
                                 
                                 HStack {
                                     Text("Your Balance")
-                                        .font(.headline)
                                         .foregroundColor(.white)
+                                        .font(.largeTitle)
+                                        .foregroundColor(.white)
+                                        .bold()
+                                        .padding(.bottom, -1)
+                                        
                                     Spacer()
-                                    Image(systemName: "creditcard")
-                                        .foregroundColor(.white)
                                 }
                                 Text("€\(viewModel.balance, specifier: "%.2f")")
                                     .font(.largeTitle)
                                     .foregroundColor(.white)
+                                    .padding(.top, 1)
+                                    .fontWeight(.semibold)
                             }
-                            .padding(25)
+                            .padding(.horizontal, 25)
+                            .padding(.bottom, 30)
                         }
                     }
-                    .frame(height: 250)
+                    .frame(height: 280)
                     .padding(.top, -50)
 
                     
                     HStack{
                         Text("Your Transactions")
-                                .font(.title)
+                            .font(.system(size: 22))
                                 .padding(.leading)
                                 .bold()
+                                .foregroundColor(Color(hex:"747476"))
                         Spacer()
-                    }
+                    }.padding(.top, 20)
                     
                     List(viewModel.transactions) { transaction in
+
                         HStack {
+                            RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+                                .frame(width:40 , height: 40)
+                            
                             VStack(alignment: .leading) {
                                 Text(transaction.name)
                                     .font(.headline)
@@ -59,11 +78,19 @@ struct HomePage: View {
                                     .foregroundColor(.gray)
                             }
                             Spacer()
-                            Text("\(transaction.amount, specifier: "%.2f")€")
-                                .font(.headline)
-                            Text(transaction.location)
-                                .font(.subheadline)
-                                .foregroundColor(.gray)
+                            VStack{
+                                HStack{
+                                    Spacer()
+                                    Text("\(transaction.amount, specifier: "%.2f")€")
+                                        .font(.headline)
+                                }
+                                HStack{
+                                    Spacer()
+                                    Text(transaction.location)
+                                        .font(.subheadline)
+                                        .foregroundColor(.gray)
+                                }
+                            }
                         }
                     }
                     .listStyle(PlainListStyle())
