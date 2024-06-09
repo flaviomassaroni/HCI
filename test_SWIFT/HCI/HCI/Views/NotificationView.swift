e171761b3eee450c6f366e09e6db22d4b7da2d95
Andrea <andrea.salinetti1@icloud.com>
2024-06-09 12:30:44 +0200

    Updated Notification View

---
+++ test_SWIFT/HCI/HCI/Views/NotificationView.swift
@@ -0,0 +1,153 @@
+import SwiftUI
+
+struct NotificationView: View {
+    var group: Group
+    var body: some View {
+        VStack(spacing:0){
+            ZStack {
+                RoundedRectangle(cornerRadius: 14)
+                    .foregroundColor(Color.blue)
+                    .frame(height: 280)
+                    .padding(.top, -50)
+                VStack {
+                    Text(group.name)
+                        .font(.largeTitle)
+                        .padding(.top, 20)
+                        .foregroundColor(Color.white)
+                        .fontWeight(.semibold)
+                    Text("\(group.startDate) ---> \(group.endDate)")
+                        .foregroundColor(.white)
+                        .font(.system(size: 18))
+                        .fontWeight(.semibold)
+                    Text("\(group.currentAmount, specifier: "%.0f")/\(group.totalAmount, specifier: "%.0f")€")
+                        .font(.system(size: 35))
+                        .padding(.top, 20)
+                        .foregroundColor(Color.white)
+                        .fontWeight(.bold)
+                    HStack {
+                        Text("\(Int((group.currentAmount / group.totalAmount) * 100))%")
+                            .font(.system(size: 17))
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                            .padding(.leading, 20)
+                        
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 14)
+                                .foregroundColor(Color.white)
+                                .frame(height: 20)
+                                .padding(.trailing, 20)
+                            ProgressBar(value: group.currentAmount / group.totalAmount)
+                                .frame(height: 20)
+                                .padding(.trailing, 20)
+                        }
+                    }
+                }
+            }
+            ScrollView{
+                ZStack{
+                    RoundedRectangle(cornerRadius: 14)
+                        
+                        
+                    HStack {
+                        Image(systemName: "exclamationmark.triangle")
+                            .resizable()
+                            .frame(width: 75, height: 70)
+                            .foregroundColor(Color(hex: "FFB800"))
+                            .padding(.vertical,10)
+                            .padding(.horizontal, 20)
+                        Spacer()
+                        VStack(alignment: .leading) {
+                            HStack{
+                                Text("Warning")
+                                    .foregroundColor(Color(hex: "6C6C6C"))
+                                    .fontWeight(.bold)
+                                    .font(.system(size: 20))
+                                Spacer()
+                            }
+                            .padding(.leading, 40)
+                            Text("You are not up to date with contributions")
+                                .foregroundColor(Color(hex: "FFB800"))
+                                .fontWeight(.semibold)
+                                .multilineTextAlignment(.center)
+                                .padding(.leading, -20)
+                        }
+                    }
+                }.frame(width: 380, height: 80)
+                    .foregroundColor(.white)
+                    .padding(.top, 20)
+                
+                Text("Contributions To Pay")
+                    .padding(.trailing, 180)
+                    .padding()
+                    .fontWeight(.semibold)
+                    .foregroundColor(Color(hex: "6C6C6C"))
+                    .font(.system(size: 20))
+                
+                ForEach(group.contributionHistory, id: \.id) { contribution in
+                    HStack {
+                        Text("\(contribution.amount, specifier: "%.0f")€")
+                            .fontWeight(.bold)
+                            .font(.system(size: 20))
+                        Spacer()
+                        Text("\(contribution.date)")
+                            .font(.system(size: 17))
+                        
+
+                    }
+                    .padding(.horizontal, 40)
+                    .padding(.vertical, 10)
+                    .background(Color.white) // Background color
+                    .cornerRadius(14) // Apply corner radius to the background
+                    
+                    .padding(.horizontal, 20)
+                }
+                ZStack{
+                    RoundedRectangle(cornerRadius: 14)
+                    HStack {
+                            Text("Total: \(10.0, specifier: "%.0f")€")
+                                .foregroundColor(Color(.black))
+                                .fontWeight(.semibold)
+                                .font(.system(size: 28))
+                                .multilineTextAlignment(.center)
+                                .padding(.leading, 35)
+                            Spacer()
+                            Button("Add Missing Contribution") {
+                            }
+                            .frame(width: 140, height: 55)
+                            .background(Color(hex:"62DF57"))
+                            .cornerRadius(14)
+                            .padding(.trailing, 15)
+                            .fontWeight(.bold)
+                            .shadow(color: .gray, radius: 5, x: 0, y: 1)
+                            
+                            
+                            
+                            }
+                   
+                }.frame(width: 380, height: 80)
+                    .foregroundColor(.white)
+                    .padding(.top, 10)
+                
+                
+                
+            }.background(Color(hex:"ECECEC"))
+               
+        }.background(Color(hex:"ECECEC"))
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView(group: Group(name: "Boat", startDate: "23/05/24", endDate: "30/06/25", period:"1M", totalAmount: 6000,  currentAmount: 550, contributionHistory: [
+            Contribution(owner: Participant(name:"Andrea Salinetti", colour: Color(hex: "FF5733")), date: "01/05/24", amount: 100.0),
+            Contribution(owner: Participant(name:"Flavio Massaroni", colour: Color(hex: "3357FF")), date: "01/05/24", amount: 100.0),
+            Contribution(owner:Participant(name:"Leonardo Scappatura", colour: Color(hex: "33FF57")), date: "01/05/24", amount: 100.0),
+            Contribution(owner: Participant(name:"Andrea Salinetti", colour: Color(hex: "FF5733")), date: "01/05/24", amount: 100.0)],
+         participants: [
+              Participant(name:"Andrea Salinetti", colour: Color(hex: "FF5733")),
+              Participant(name:"Andrea Salinetti", colour: Color(hex: "33FF57")),
+              Participant(name:"Andrea Salinetti", colour: Color(hex: "3357FF"))
+        ]))
+    }
+}
+
